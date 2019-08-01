a63ea0c653d5a6d60638778234d2435d2bb9755f
Alexander Militar <militara106@gmail.com>
2019-07-31 19:21:03 -0700

    Javascript bug fixes and consolidation, all buttons created dynamically

--- app.js
+++ app.js
@@ -3,21 +3,27 @@ $(document).ready(function () {
     // Set initial tags
     var buttons = ['dog', 'cat', 'rabbit', 'hamster', 'skunk', 'goldfish', 'bird', 'ferret', 'turtle', 'sugar glider', 'chinchilla', 'hedgehog', 'hermit crab', 'gerbil', 'pygmy goat', 'chicken', 'capybara', 'teacup pig', 'serval', 'manbearpig'];
 
+        for(var i = 0; i<buttons.length; i++){
+        var button = $("<button>");
+        button.text(buttons[i]);
+        button.attr("data-animal", buttons[i]);
+        $("#button-container").append(button);
+        console.log("Initial buttons created");
+        }
+
     // Create New Tag on submit
     $("#submit").on("click", function () {
         event.preventDefault();
-        var input = $("#input").val().trim();
-        console.log("User Input: " + input);
+        console.log("User Input: " + $("#input").val().trim().toLowerCase());
 
-        if (buttons.includes(input) == false && input != '') {
+        if (buttons.includes($("#input").val().trim().toLowerCase()) == false && $("#input").val().trim().toLowerCase() != '') {
             // Create new tag button
-            input = input.toLowerCase();
             var button = $("<button>");
-            button.text(input);
-            button.attr("data-animal", input);
+            button.text($("#input").val().trim().toLowerCase());
+            button.attr("data-animal", $("#input").val().trim().toLowerCase());
             $("#button-container").append(button);
-            buttons.push(input);
-            console.log("button created for " + input);
+            buttons.push($("#input").val().trim().toLowerCase());
+            console.log("button created for " + $("#input").val().trim().toLowerCase());
         }
 
         // Empty form
@@ -27,14 +33,10 @@ $(document).ready(function () {
     // Click Tag Button
     $("#button-container").on("click", 'button', function () {
 
-        //API key
-        var key = "QLPRNUPvimJ8IElJtlEdGsKdheyaZdQM";
-
-        //Tag
-        var tag = $(this).attr("data-animal");
+        //API key  "QLPRNUPvimJ8IElJtlEdGsKdheyaZdQM";
 
         //QueryURL
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + tag + "&api_key=" + key + "&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).attr("data-animal") + "&api_key=" + "QLPRNUPvimJ8IElJtlEdGsKdheyaZdQM" + "&limit=10";
         console.log(queryURL);
 
         //API
@@ -51,7 +53,7 @@ $(document).ready(function () {
                 for (var i = 0; i < results.length; i++) {
                     // Create new objects
                     var newDiv = $("<div>");
-                    var p = $("<p>").text("Rating: " + results[i].rating.toUpperCase());
+                    // var p = $("<p>").text("Rating: " + results[i].rating.toUpperCase());
                     var image = $("<img>");
 
                     // Set image attributes
@@ -67,7 +69,7 @@ $(document).ready(function () {
 
                     // Append items to div and div to container
                     newDiv.append(image);
-                    newDiv.append(p);
+                    newDiv.append($("<p>").text("Rating: " + results[i].rating.toUpperCase()));
                     $("#gif-container").prepend(newDiv);
                     console.log("Gif created");
                 }
@@ -78,16 +80,16 @@ $(document).ready(function () {
 
     // Function to pause and play gif
     $("#gif-container").on("click", ".gif", function () {
-        var dataState = $(this).attr("data-state");
-        var dataAnimate = $(this).attr("data-animate");
-        var dataStill = $(this).attr("data-still");
 
-        if (dataState == "still") {
-            $(this).attr("src", dataAnimate);
+        // If still, animate
+        if ($(this).attr("data-state") == "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
             $(this).attr("data-state", "animate");
             console.log("Gif play");
-        } else {
-            $(this).attr("src", dataStill);
+        }
+        // If animate, still
+        else {
+            $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state", "still");
             console.log("GIf pause");
         }
