c2ad106d5f19850b7dc9bb498d363239ee3901ab
Alexander Militar <militara106@gmail.com>
2019-07-31 19:08:45 -0700

    Completed js, html, and css

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 $(document).ready(function () {
-    var buttons = ['dog','cat','rabbit','hamster','skunk','goldfish','bird','ferret','turtle','sugar glider','chinchilla','hedgehog','hermit crab','gerbil','pygmy goat','chicken','capybara','teacup pig','serval','manbearpig'];
 
-    // Create New Tag
+    // Set initial tags
+    var buttons = ['dog', 'cat', 'rabbit', 'hamster', 'skunk', 'goldfish', 'bird', 'ferret', 'turtle', 'sugar glider', 'chinchilla', 'hedgehog', 'hermit crab', 'gerbil', 'pygmy goat', 'chicken', 'capybara', 'teacup pig', 'serval', 'manbearpig'];
+
+    // Create New Tag on submit
     $("#submit").on("click", function () {
         event.preventDefault();
         var input = $("#input").val().trim();
         console.log("User Input: " + input);
 
-        if (buttons.includes(input)==false && input !='') {
+        if (buttons.includes(input) == false && input != '') {
             // Create new tag button
             input = input.toLowerCase();
             var button = $("<button>");
@@ -15,6 +17,7 @@ $(document).ready(function () {
             button.attr("data-animal", input);
             $("#button-container").append(button);
             buttons.push(input);
+            console.log("button created for " + input);
         }
 
         // Empty form
@@ -23,6 +26,7 @@ $(document).ready(function () {
 
     // Click Tag Button
     $("#button-container").on("click", 'button', function () {
+
         //API key
         var key = "QLPRNUPvimJ8IElJtlEdGsKdheyaZdQM";
 
@@ -45,22 +49,50 @@ $(document).ready(function () {
                 var results = response.data;
 
                 for (var i = 0; i < results.length; i++) {
+                    // Create new objects
                     var newDiv = $("<div>");
-                    var p = $("<p>").text("Rating: " + results[i].rating);
+                    var p = $("<p>").text("Rating: " + results[i].rating.toUpperCase());
                     var image = $("<img>");
 
-                    image.attr("src", results[i].images.fixed_height.url);
+                    // Set image attributes
+                    image.attr("src", results[i].images.fixed_height_still.url);
+                    image.attr("class", "gif");
+                    image.attr("data-still", results[i].images.fixed_height_still.url);
+                    image.attr("data-animate", results[i].images.fixed_height.url);
+                    image.attr("data-state", "still");
+
+                    // Style div
                     newDiv.css("display", "inline-block");
                     newDiv.css("padding", "1rem");
 
-                    newDiv.append(p);
+                    // Append items to div and div to container
                     newDiv.append(image);
-
+                    newDiv.append(p);
                     $("#gif-container").prepend(newDiv);
+                    console.log("Gif created");
                 }
             });
 
 
     });
+
+    // Function to pause and play gif
+    $("#gif-container").on("click", ".gif", function () {
+        var dataState = $(this).attr("data-state");
+        var dataAnimate = $(this).attr("data-animate");
+        var dataStill = $(this).attr("data-still");
+
+        if (dataState == "still") {
+            $(this).attr("src", dataAnimate);
+            $(this).attr("data-state", "animate");
+            console.log("Gif play");
+        } else {
+            $(this).attr("src", dataStill);
+            $(this).attr("data-state", "still");
+            console.log("GIf pause");
+        }
+
+    });
+
 
 });
